62732d069b844e414667f07c34a322a749fcf273
Kent Ng <kent.ng201@gmail.com>
2023-02-18 17:44:48 +0800

    fix for _rev typescript for model

--- src/definitions/Model.ts
+++ src/definitions/Model.ts
@@ -1,5 +1,5 @@
 // reference from: https://medium.com/dailyjs/typescript-create-a-condition-based-subset-types-9d902cea5b8c
-export type ReservedFieldForSelect = 'cName' | 'dName' | 'needTimestamp' | 'relationships' | '_dirty' | 'formatResponse' | '_id';
+export type ReservedFieldForSelect = 'cName' | 'dName' | 'needTimestamp' | 'relationships' | '_dirty' | 'formatResponse' | '_id' | '_rev';
 export type FunctionlessModel<T> = Omit<Omit<T, {
     [Key in keyof T]: T[Key] extends Function ? Key : never;
 }[keyof T]>, ReservedFieldForSelect>;
