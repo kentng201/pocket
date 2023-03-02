ad3d8427d365bfbdd2fd666ba03199d962ac1add
Kent Ng <kent.ng201@gmail.com>
2023-03-02 21:35:15 +0800

    add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
         '@typescript-eslint/no-inferrable-types': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        'no-async-promise-executor': 'off',
     },
 };
