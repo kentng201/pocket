2b4a5d81cee2e870a8eea3cc3a41f2558408e7b2
Kent Ng <kent.ng201@gmail.com>
2023-03-04 12:19:19 +0800

    add model rev checking spec

---
+++ spec/Model.set-deleted-ref.spec.ts
@@ -0,0 +1,36 @@
+import { DatabaseManager } from 'src/manager/DatabaseManager';
+import { Model } from '../src/model/Model';
+
+const dbName = 'model-hooks';
+
+describe('Model', () => {
+    class User extends Model {
+        static dbName = dbName;
+
+        name!: string;
+        password?: string;
+
+        getRandomPassword() {
+            return Math.random().toString();
+        }
+    }
+
+    beforeEach(async () => {
+        await DatabaseManager.connect(dbName, { dbName, adapter: 'memory', silentConnect: true });
+    });
+
+    it('should not be able to save new reference', async () => {
+        const user = new User;
+        user.name = 'John';
+        await user.save();
+
+        const clonedUser = {...user};
+        await user.delete();
+
+        const savingClonedUser = new User;
+        savingClonedUser.fill(clonedUser);
+        await savingClonedUser.save();
+        expect(savingClonedUser._rev).not.toBe(clonedUser._rev);
+
+    });
+});
