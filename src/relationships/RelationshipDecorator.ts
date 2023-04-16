24956851b15b0e52d7099766ee733f5cf4804b96
Kent Ng <kent.ng201@gmail.com>
2023-04-16 18:19:15 +0800

    remove strict type script for relationship decorator

--- src/relationships/RelationshipDecorator.ts
+++ src/relationships/RelationshipDecorator.ts
@@ -1,6 +1,6 @@
 import { RelationshipType } from 'src/definitions/RelationshipType';
 import { BaseModel } from '../model/Model';
-import { ModelKey, ModelStatic } from 'src/definitions/Model';
+import { ModelStatic } from 'src/definitions/Model';
 
 export type RelationshipParams = [RelationshipType, ModelStatic<BaseModel>[], Array<string>];
 
@@ -18,7 +18,7 @@ export function getRelationships<T extends BaseModel>(model: T): ForeignTypeMapp
     return foreignKeys[model.cName] || {};
 }
 
-export function BelongsTo<R extends BaseModel>(relationship: ModelStatic<R>, localKey?: string, foreignKey?: ModelKey<R>) {
+export function BelongsTo<R extends BaseModel>(relationship: ModelStatic<R>, localKey?: string, foreignKey?: string) {
     return function <T extends BaseModel>(target: T, propertyKey: string) {
         if (!target.relationships) {
             target.relationships = {};
@@ -30,7 +30,7 @@ export function BelongsTo<R extends BaseModel>(relationship: ModelStatic<R>, loc
     };
 }
 
-export function HasOne<R extends BaseModel>(relationship: ModelStatic<R>, localKey?: string, foreignKey?: ModelKey<R>) {
+export function HasOne<R extends BaseModel>(relationship: ModelStatic<R>, localKey?: string, foreignKey?: string) {
     return function <T extends BaseModel>(target: T, propertyKey: string) {
         if (!target.relationships) {
             target.relationships = {};
@@ -42,7 +42,7 @@ export function HasOne<R extends BaseModel>(relationship: ModelStatic<R>, localK
     };
 }
 
-export function HasMany<R extends BaseModel>(relationship: ModelStatic<R>, localKey?: string, foreignKey?: ModelKey<R>) {
+export function HasMany<R extends BaseModel>(relationship: ModelStatic<R>, localKey?: string, foreignKey?: string) {
     return function <T extends BaseModel>(target: T, propertyKey: string) {
         if (!target.relationships) {
             target.relationships = {};
@@ -54,7 +54,7 @@ export function HasMany<R extends BaseModel>(relationship: ModelStatic<R>, local
     };
 }
 
-export function BelongsToMany<R extends BaseModel, P extends BaseModel>(relationship: ModelStatic<R>, pivot: ModelStatic<P>, localKey?: string, foreignKey?: ModelKey<R>, through?: string) {
+export function BelongsToMany<R extends BaseModel, P extends BaseModel>(relationship: ModelStatic<R>, pivot: ModelStatic<P>, localKey?: string, foreignKey?: string, through?: string) {
     return function <T extends BaseModel>(target: T, propertyKey: string) {
         if (!target.relationships) {
             target.relationships = {};
