0bfa82a3920cdfbb25e4d44d761e3768e4b91b02
Kent Ng <kent.ng201@gmail.com>
2023-04-17 01:02:06 +0800

    export model decorator "PocketModel"

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from 'src/query-builder/QueryBuilder';
 export * from 'src/real-time/RealTimeModel';
 export * from 'src/real-time/DatabaseSync';
 export * from 'src/relationships/RelationshipDecorator';
+export * from 'src/model/ModelDecorator';
 export * from 'src/boot';
 
 import { boot } from 'src/boot';
