4cef5fc153ec4e3b1094f4f430d2887ab03c0adc
Kent Ng <kent.ng201@gmail.com>
2023-06-22 15:01:31 +0800

    make two way sync working

--- src/real-time/DatabaseSync.ts
+++ src/real-time/DatabaseSync.ts
@@ -6,40 +6,10 @@ export function syncDatabases(...dbNames: string[]) {
     let tempDb: PouchDB.Database & DatabaseCustomConfig;
     databases.forEach(db => {
         if (tempDb && db) {
-            if (tempDb.password) {
-                tempDb
-                    .changes({
-                        since: 'now',
-                        include_docs: true,
-                        live: true,
-                    })
-                    .on('change', async (info: PouchDB.Core.ChangesResponseChange<any>) => {
-                        try {
-                            const doc = info.doc;
-                            console.log('doc: ', doc);
-                            if (doc._id && doc._rev && Object.keys(doc).length == 2) {
-                                // deleted doc
-                                return;
-                            }
-                            await db.get(doc._id).then(async (result) => {
-                                if (result && result._rev) {
-                                    const newDoc = { ...doc, _rev: result._rev, };
-                                    await db.put(newDoc);
-                                }
-                            }).catch(async () => {
-                                await db.put({ ...doc, _rev: undefined, });
-                            });
-                        } catch (err) {
-                            console.log('err: ', err);
-                        }
-                    });
-            } else {
-                console.log('goes else');
-                tempDb.sync(db, {
-                    live: true,
-                    retry: true,
-                });
-            }
+            tempDb.sync(db, {
+                live: true,
+                retry: true,
+            });
         }
         if (db) {
             tempDb = db;
