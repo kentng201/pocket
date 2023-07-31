17496ef260b51f2f8d606c25c1f4c34916bcd387
Kent Ng <kent.ng201@gmail.com>
2023-07-31 09:23:20 +0800

    add unsyncDatabases api

--- src/real-time/DatabaseSync.ts
+++ src/real-time/DatabaseSync.ts
@@ -1,18 +1,30 @@
 import { DatabaseCustomConfig, DatabaseManager } from '..';
 
+const syncMap = new Map<string, PouchDB.Replication.Sync<{}>>();
 
 export function syncDatabases(...dbNames: string[]) {
     const databases = dbNames.map(dbName => DatabaseManager.get(dbName));
     let tempDb: PouchDB.Database & DatabaseCustomConfig;
     databases.forEach(db => {
         if (tempDb && db) {
-            tempDb.sync(db, {
+            const sync = tempDb.sync(db, {
                 live: true,
                 retry: true,
             });
+            syncMap.set(tempDb.name, sync);
+            syncMap.set(db.name, sync);
         }
         if (db) {
             tempDb = db;
+        }
+    });
+}
+
+export function unsyncDatabases(...dbNames: string[]) {
+    dbNames.forEach(dbName => {
+        const sync = syncMap.get(dbName);
+        if (sync) {
+            sync.cancel();
         }
     });
 }
