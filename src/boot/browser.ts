ded0aa5a09c3839425168d0cca27116ebded64d2
Kent Ng <kent.ng201@gmail.com>
2023-08-07 11:01:36 +0800

    update browser have fallback mech to set the config

--- src/boot/browser.ts
+++ src/boot/browser.ts
@@ -1,6 +1,18 @@
 import { replaceEnvVariable, setupConfig, ConfigPersistor } from 'src/boot';
+import { MultiPocketConfig, SinglePocketConfig } from 'src/definitions/boot';
+
+export async function setConfig(config: SinglePocketConfig | MultiPocketConfig) {
+    ConfigPersistor.set(config);
+    return config;
+}
+export function hasPersistedConfig() {
+    return !!ConfigPersistor.get();
+}
 
 async function getConfig() {
+    if (hasPersistedConfig()) {
+        return ConfigPersistor.get();
+    }
     const configFilePath = 'pocket.config.json';
     try {
         const file = await fetch(configFilePath);
