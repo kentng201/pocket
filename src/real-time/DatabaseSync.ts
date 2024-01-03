9afe98d31f4b2ef8aec9c9716cea2b5a0e23b9c5
Kent Ng <kent.ng201@gmail.com>
2024-01-03 16:40:52 +0800

    fix sync too much memory leak issue

--- src/real-time/DatabaseSync.ts
+++ src/real-time/DatabaseSync.ts
@@ -5,12 +5,24 @@ const syncMap = new Map<string, PouchDB.Replication.Sync<{}>>();
 export function syncDatabases(...dbNames: string[]) {
     const databases = dbNames.map(dbName => DatabaseManager.get(dbName));
     let tempDb: PouchDB.Database & DatabaseCustomConfig;
+
+    // to cancel all previous syncs for prevent memory leak
+    databases.forEach(db => {
+        if (db) {
+            const haveSync = syncMap.get(db.name);
+            if (haveSync) {
+                haveSync.cancel();
+            }
+        }
+    });
+
     databases.forEach(db => {
         if (tempDb && db) {
             const sync = tempDb.sync(db, {
                 live: true,
                 retry: true,
             });
+
             syncMap.set(tempDb.name, sync);
             syncMap.set(db.name, sync);
         }
