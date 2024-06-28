36950f2a42f6a88d4495ad88700e784d07f6c6e8
Kent Ng <kent.ng201@gmail.com>
2024-06-29 00:28:49 +0800

    add PocketDefault for easy setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { setEnvironment } from '.';
+import { setRealtime } from '.';
+
 export * from 'src/manager/DatabaseManager';
 export * from 'src/manager/RepoManager';
 
@@ -8,3 +11,8 @@ export * from 'src/real-time/DatabaseSync';
 export * from 'src/relationships/RelationshipDecorator';
 export * from 'src/model/ModelDecorator';
 export * from 'src/helpers/Persistor';
+
+export const PocketDefault = {
+    setRealtime: setRealtime,
+    setEnvironment: setEnvironment,
+};
